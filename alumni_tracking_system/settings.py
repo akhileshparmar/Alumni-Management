0a2c1ad7cd7f76eee14f8a2602170847277214ee
Akhilesh <64184042+akhiparmar@users.noreply.github.com>
2021-01-13 18:11:12 +0530

    EMAIL_HOST Username/Password removed

--- alumni_tracking_system/settings.py
+++ alumni_tracking_system/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER="sage.alumni.tracking.system@gmail.com"
-EMAIL_HOST_PASSWORD="RN5P@sage.alumni.tracking.system@"
+EMAIL_HOST_USER="***********"
+EMAIL_HOST_PASSWORD="**************"
